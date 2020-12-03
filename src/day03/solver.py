638706090ff9a562530a9ce7d0ea7617f53f1ad8
Mattias Liljeström <mattias.liljestrom@evry.com>
2020-12-03 21:20:44 +0100

    day 03 - small updates

--- src/day03/solver.py
+++ src/day03/solver.py
@@ -4,21 +4,19 @@ from functools import reduce
 
 def a(i, dx, dy):
     trees = 0
-    mx = len(i[0])
     x = 0
     for y in range(dy, len(i), dy):
-        x = (x + dx) % mx
+        x = (x + dx) % len(i[0])
         if i[y][x] == '#':
             trees += 1
 
     return trees
 
 
-def b(i):
-    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-    return reduce(lambda x, y: x * y, map(lambda s: a(i, s[0], s[1]), slopes))
+def b(i, slopes):
+    return reduce(lambda x, y: x * y, map(lambda slope: a(i, *slope), slopes))
 
 
 i = read_file('day03/1.in').splitlines()
-print(a(i, 3, 1), b(i))
+print(a(i, *[3, 1]), b(i, [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]))
 
