875bc4a2132ca42fc3e4eb5fad928765c409bb6f
Mattias Liljeström <mattias.liljestrom@evry.com>
2020-12-18 08:06:23 +0100

    day17 - remove duplications

--- src/day17/solver.py
+++ src/day17/solver.py
@@ -1,71 +1,34 @@
 from utils.file import read_file
+import itertools
+from operator import add
 
 
-def get_neighbors_3d(x, y, z):
+def get_neighbors(cube, dimension):
     answer = set()
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            for dz in [-1, 0, 1]:
-                if dx != 0 or dy != 0 or dz != 0:
-                    answer.add((x+dx, y+dy, z+dz))
+    for d in itertools.product([-1, 0, 1], repeat=dimension):
+        answer.add(tuple(list(map(add, cube, d))))
+    answer.remove(cube)
     return answer
 
 
-def star_a(initial_state):
-    z = 0
+def solve(initial_state, dimension, cycles=6):
+    zeros = [0] * (dimension - 2)
     active_cubes = set()
     for y, row in enumerate(initial_state.splitlines()):
         for x in [x for x, c in enumerate(list(row)) if c == '#']:
-            active_cubes.add((x, y, z))
+            active_cubes.add((x, y, *zeros))
 
-    for cycle in range(6):
+    for cycle in range(cycles):
         next_cycle_active_cubes = set()
         inactive_cubes = set()
         for cube in active_cubes:
-            neighbors = get_neighbors_3d(*cube)
+            neighbors = get_neighbors(cube, dimension)
             if len(active_cubes.intersection(neighbors)) in [2, 3]:
                 next_cycle_active_cubes.add(cube)
             inactive_cubes |= (neighbors - active_cubes)
 
         for inactive_cube in inactive_cubes:
-            if len(active_cubes.intersection(get_neighbors_3d(*inactive_cube))) == 3:
-                next_cycle_active_cubes.add(inactive_cube)
-
-        active_cubes = next_cycle_active_cubes
-
-    return len(active_cubes)
-
-
-def get_neighbors_4d(x, y, z, w):
-    answer = set()
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            for dz in [-1, 0, 1]:
-                for dw in [-1, 0, 1]:
-                    if dx != 0 or dy != 0 or dz != 0 or dw != 0:
-                        answer.add((x+dx, y+dy, z+dz, w+dw))
-
-    return answer
-
-
-def star_b(initial_state):
-    z, w = 0, 0
-    active_cubes = set()
-    for y, row in enumerate(initial_state.splitlines()):
-        for x in [x for x, c in enumerate(list(row)) if c == '#']:
-            active_cubes.add((x, y, z, w))
-
-    for cycle in range(6):
-        next_cycle_active_cubes = set()
-        inactive_cubes = set()
-        for cube in active_cubes:
-            neighbors = get_neighbors_4d(*cube)
-            if len(active_cubes.intersection(neighbors)) in [2, 3]:
-                next_cycle_active_cubes.add(cube)
-            inactive_cubes |= (neighbors - active_cubes)
-
-        for inactive_cube in inactive_cubes:
-            if len(active_cubes.intersection(get_neighbors_4d(*inactive_cube))) == 3:
+            if len(active_cubes.intersection(get_neighbors(inactive_cube, dimension))) == 3:
                 next_cycle_active_cubes.add(inactive_cube)
 
         active_cubes = next_cycle_active_cubes
@@ -74,5 +37,5 @@ def star_b(initial_state):
 
 
 data = read_file('day17/1.in')
-print(star_a(data), star_b(data))
+print(solve(data, 3), solve(data, 4))
 
